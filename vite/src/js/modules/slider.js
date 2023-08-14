3ad914764c05add7278d3b06821d5252795400db
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-08-14 13:42:58 +0500

    interior

--- vite/src/js/modules/slider.js
+++ vite/src/js/modules/slider.js
@@ -73,3 +73,31 @@ if (slides.length > 1) {
 }
 
 initSlider();
+
+
+const interiorSlider = new Swiper('.interior-slider', {
+	modules: [Pagination, Parallax],
+	slidesPerView: 3,
+	spaceBetween: 20,
+	pagination: {
+		el: ".interior-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		// when window width is >= 320px
+		320: {
+		  slidesPerView: 1,
+		  spaceBetween: 10
+		},
+		// when window width is >= 480px
+		600: {
+		  slidesPerView: 2,
+		  spaceBetween: 10
+		},
+		// when window width is >= 640px
+		768: {
+		  slidesPerView: 3,
+		  spaceBetween: 20
+		}
+	  }
+})
