aa5780c3b50eaf48e2a86ef6b84cfb0ba20d717b
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-08-15 15:59:49 +0500

    characteristics block

--- vite/src/js/app.js
+++ vite/src/js/app.js
@@ -9,3 +9,19 @@ import "./modules/modals";
 import GLightbox from 'glightbox';
 
 const lightbox = GLightbox();
+
+
+const spoilerLinks = document.querySelectorAll('.spoiler-link');
+if(spoilerLinks.length){
+    spoilerLinks.forEach(function(link){
+        const parent = link.closest('.spoiler-item');
+        const content = parent.querySelector('.spoiler-content');
+        const iconPlus = parent.querySelector('svg.plus');
+        const iconMinus = parent.querySelector('svg.minus');
+        link.onclick = () => {
+            content.style.display = content.style.display === 'none' ? 'block' : 'none';
+            iconPlus.style.display = iconPlus.style.display === 'none' ? 'block' : 'none';
+            iconMinus.style.display = iconMinus.style.display === 'none' ? 'block' : 'none';
+        }
+    })
+}
