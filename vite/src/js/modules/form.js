f9e7c63f51d38c80d904edb855515efa95e6b27f
Alexander Sabanov <alexsab@mail.ru>
2023-09-01 12:13:58 +0400

    add dataLayer lead

--- vite/src/js/modules/form.js
+++ vite/src/js/modules/form.js
@@ -1,6 +1,12 @@
 const $$ = (el) => {
 	return document.querySelectorAll(el);
 };
+function dl(event, t = {}) {
+	void 0 !== window.dataLayer && window.dataLayer.push({
+		event: event,
+		...t
+	})
+}
 // PHONE MASK
 function maskphone(e) {
 	let num = this.value
@@ -138,7 +144,13 @@ $$("form").forEach((form) => {
 		for (const pair of formData) {
 			params.append(pair[0], pair[1]);
 		}
-		// await fetch('https://alexsab.ru/lead/test/', {
+
+		let formDataObj = {"EventProperties":{}};
+		formData.forEach((value, key) => (formDataObj["EventProperties"][key] = value));
+		formDataObj['EventCategory'] = 'Lead';
+		formDataObj["EventProperties"]['formID'] = form.id;
+		formDataObj['sourceName'] = 'page';
+
 		await fetch("https://alexsab.ru/lead/livan/orenburg/", {
 			method: "POST",
 			mode: "cors",
@@ -159,6 +171,7 @@ $$("form").forEach((form) => {
 				} else if (data.answer == "error") {
 					showMessageModal(messageModal, errorIcon, errorText + "<br>" + data.error);
 				} else {
+					dl("form_success", formDataObj)
 					showMessageModal(messageModal, successIcon, successText);
 				}
 				form.reset();
