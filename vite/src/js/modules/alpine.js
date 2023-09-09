3d3ad2ca2d85493331535405a56be5f7964d5760
Anatoly Zhuravlev <newstreetpunk@yandex.ru>
2023-09-09 22:16:15 +0500

    cookie & scroll up btn

--- vite/src/js/modules/alpine.js
+++ vite/src/js/modules/alpine.js
@@ -1,5 +1,32 @@
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
  
 // window.Alpine = Alpine
+document.addEventListener("alpine:init", ()=>{
+    Alpine.data("cookieAgreement", ()=>({
+            show: localStorage.getItem('cookie') || false,
+            onClick() {
+                localStorage.setItem('cookie', true);
+                this.$root.remove();
+            }
+        })
+    );
+    Alpine.data("scrollTop", t=>({
+        scrolled: !1,
+            init() {
+                this.scrolled = document.documentElement.scrollTop > window.innerHeight / 1,
+                document.addEventListener("scroll", e=>this.onScroll(e))
+            },
+            onScroll(e) {
+                this.scrolled = document.documentElement.scrollTop > window.innerHeight / 1
+            },
+            onClick() {
+                document.documentElement.scroll({
+                    top: 0,
+                    behavior: "smooth"
+                })
+            }
+        })
+    );
+});
  
-Alpine.start()
+Alpine.start();
