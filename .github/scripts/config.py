a243a6c7406a9098c6cafc21ca1aeddab55edc0d
Alexander Sabanov <alexsab@mail.ru>
2024-07-18 15:22:59 +0400

    add config

---
+++ .github/scripts/config.py
@@ -0,0 +1,35 @@
+dealer = {
+    "city": "Оренбург",
+    "where": "Оренбурге",
+}
+# Словарь соответствия цветов
+model_mapping = {
+    "X3 Pro": {
+        "folder": "x3pro",
+        "color": {
+            "Beige": "gold.webp",
+            "White": "white.webp",
+            "Red": "red.webp",
+            "Blue": "blue.webp",
+            "Gray": "gray.webp",
+            "Grey": "gray.webp",
+        }},
+    "X6 Pro": {
+        "folder": "x6pro",
+        "color": {
+            "White": "white.webp",
+            "Blue": "blue.webp",
+            "Gray": "gray.webp",
+            "Grey": "gray.webp",
+        }},
+    "S6 Pro": {
+        "folder": "s3pro",
+        "color": {
+            "White": "white.webp",
+            "Blue": "blue.webp",
+            "Gray": "gray.webp",
+            "Grey": "gray.webp",
+        }},
+
+    # ... добавьте другие модели по мере необходимости
+}
